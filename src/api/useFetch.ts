bcd6aeea694b37dccfe5c0c964f57cc221f18f9b
Bruno <brunojparisz@gmail.com>
2024-03-12 15:22:19 -0300

    Formated usefetch

--- src/api/useFetch.ts
+++ src/api/useFetch.ts
@@ -11,21 +11,20 @@ export function useFetch(url: string) {
       try {
         const response = await fetch(url);
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error("Network response was not ok");
         }
         const jsonData = await response.json();
         setData(jsonData);
       } catch (error) {
         if (error instanceof Error) {
-           setError(error.message || 'An error occurred');
-         }
+          setError(error.message || "An error occurred");
+        }
       } finally {
         setLoading(false);
       }
     };
 
     fetchData();
-
   }, [url]);
 
   return { data, loading, error };
